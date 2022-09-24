c97df8c345eb50b0332dd21318c9631a6ef7c618
Abid <mohdabidaliusa@gamil.com>
2022-09-23 18:03:38 -0700

    IC

---
+++ dataImport.js
@@ -0,0 +1,12 @@
+import fetch from 'node-fetch';
+
+const apiURL = 'https://swapi.dev/api/people'
+
+fetch(apiURL)
+    .then(res => res.json())
+    .then(json => {
+        console.log(json);
+        console.log(`Data was retrieved succesfully`);
+    });
+
+console.log('Getting data');
